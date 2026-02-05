d8c47835439e68f49f824c869905f5d3ab87e00e
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-05 15:28:50 +0100

    contact_manager(java): add id for each contact

--- src/contact_manager/Contact.java
+++ src/contact_manager/Contact.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 // Class representing a contact with various attributes such as name, phone number, email, address, etc.
 public class Contact {
 
+  private static int nextId = 1; // Static variable to generate unique IDs for contacts
+  private final int id; // Unique identifier for each contact
   private String lastname;
   private String firstname;
   private String phoneNumber;
@@ -21,6 +23,7 @@ public class Contact {
   public Contact(String lastname, String firstname, String phoneNumber, String email, String address, String city,
       String state,
       String zipCode, String country, String company, LocalDate birthday) {
+    this.id = nextId++; // Assign a unique ID and increment the static counter
     this.lastname = lastname;
     this.firstname = firstname;
     this.phoneNumber = phoneNumber;
@@ -35,10 +38,12 @@ public class Contact {
 
   }
 
-  // Returns a formatted multi-line string representation of the contact for console display.
+  // Returns a formatted multi-line string representation of the contact for
+  // console display.
   public String toString() {
     return """
         Contact:
+        ID: %d
         Lastname: %s
         Firstname: %s
         Phone Number: %s
@@ -50,7 +55,7 @@ public class Contact {
         Country: %s
         Company: %s
         Birthday: %s
-        """.formatted(lastname, firstname, phoneNumber, email, address, city, state, zipCode, country, company,
+        """.formatted(id, lastname, firstname, phoneNumber, email, address, city, state, zipCode, country, company,
         birthday);
   }
 }
